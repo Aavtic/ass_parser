a91697baaf168340d11eb46142f9d12124fc74b1
Aavtic <aadishm79@gmail.com>
2024-08-31 23:43:44 +0530

    Parser for .srt files.

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,32 @@
+struct SrtFile;
+struct SrtData {
+    index: String,
+    start: String,
+    end: String,
+}
+
+
+
+
 pub fn parse_srt(contents: String) {
-    let splitted = contents.split("\r\n");
-    println!("{:?}", splitted);
+    let splitted: Vec<&str> = contents.split("\r\n").collect();
+    let mut segments = Vec::new();
+    let mut start = false;
+    let mut current_buffer = Vec::new();
+
+    for line in splitted {
+        println!("line: {}, {}", line, line == "");
+        if line == "" {
+            if !start {
+                start = true;
+            } else {
+                start = false;
+                segments.push(current_buffer);
+                current_buffer = Vec::new();
+            }
+        } else {
+            current_buffer.push(line);
+        }
+    }
+    println!("s{:?}e", segments[0]);
 }
