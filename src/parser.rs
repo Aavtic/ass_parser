bd910de9b4dee12949ec19b368f553f4dd7fa61d
Aavtic <aadishm79@gmail.com>
2024-09-04 17:06:20 +0530

    Updates.

--- src/parser.rs
+++ src/parser.rs
@@ -3,30 +3,63 @@ struct SrtData {
     index: String,
     start: String,
     end: String,
+    text: String,
 }
 
+type Segments = Vec<Vec<String>>;
+type SrtContent = Vec<SrtData>;
 
+impl SrtData {
+    fn get_srt_segments(&self, srt_data: String) -> Segments {
+        let splitted = srt_data.lines();
+        let mut segments = Vec::new();
+        let mut current_buffer: Vec<String> = Vec::new();
 
-
-pub fn parse_srt(contents: String) {
-    let splitted: Vec<&str> = contents.split("\r\n").collect();
-    let mut segments = Vec::new();
-    let mut start = false;
-    let mut current_buffer = Vec::new();
-
-    for line in splitted {
-        println!("line: {}, {}", line, line == "");
-        if line == "" {
-            if !start {
-                start = true;
+        for line in splitted {
+            // println!("line: {}, {}", line, line.is_empty());
+            if line.is_empty() {
+                    segments.push(current_buffer);
+                    current_buffer = Vec::new();
             } else {
-                start = false;
-                segments.push(current_buffer);
-                current_buffer = Vec::new();
+                current_buffer.push(line.to_owned());
             }
-        } else {
-            current_buffer.push(line);
+        }
+
+        return segments;
+    }
+
+    fn parse_timestamps(&self, timestamps: String) -> Vec<String> {
+        let timestamp_splitted: Vec<&str> = timestamps.split(" --> ").collect();
+        let timestamps_str: Vec<String> = timestamp_splitted.iter().map(|s| s.to_string()).collect();
+        assert_eq!(2, timestamps.len());
+
+        return timestamps_str;
+    }
+
+    fn get_srt(&self, srt_data: Segments) -> SrtContent {
+        let mut text = String::new();
+        for data in srt_data{
+            let mut srt_data = data.iter();
+            let index = srt_data.next().unwrap();
+            let timestamps = &self.parse_srt(srt_data.next().unwrap().to_string());
+
+            for srt_text in srt_data {
+                text.push_str(srt_text.as_str());
+            }
+
+            let srt_data = SrtData {
+                index: index,
+
+            }
+
         }
     }
-    println!("s{:?}e", segments[0]);
+}
+
+impl SrtData {
+    pub fn parse_srt(&self, contents: String) {
+        let mut text = String::new();
+        let segments = self.get_srt_segments(contents);
+
+    }
 }
