35beb173170fb872484e6ace6f3774b5ad4238e0
Aavtic <aadishm79@gmail.com>
2024-09-10 18:25:23 +0530

    Added docs

--- src/lib.rs
+++ src/lib.rs
@@ -130,6 +130,33 @@
 ///event.add_dialogue(dialogue);
 /// ```
 ///
+/// # Modify Existing ASS files.
+///
+/// Use the `from_file` function of AssFile to modify and change the contents or appearance. 
+///
+/// ``` rust
+/// use ass_parser::{AssFile, Dialogue, AssFileOptions};
+/// use hex_color::HexColor;
+/// 
+/// fn main() -> Result<(), std::io::Error>{
+///     let mut ass_file = AssFile::from_file("subtitles.ass")?;
+///     let dialogue = Dialogue::default()
+///         .set_text("Hello Friend!");
+///     let primary_color = AssFileOptions::get_ass_color(HexColor::RED);
+/// 
+/// 
+///     ass_file.components.v4
+///         .set_primarycolour(&primary_color);
+///         
+///     ass_file.components.events
+///         .add_dialogue(dialogue);
+/// 
+///     AssFile::save_file(&ass_file, "new_subtitles.ass");
+/// 
+///     Ok(())
+/// }
+/// ```
+///
 /// # Added Support for SubRip files.
 ///
 /// Now you can load `.srt` files and convert them to `.ass` files and even modify them on the
@@ -1492,8 +1519,8 @@ impl AssFile {
     /// use ass_parser::{AssFile, V4Format, AssFileOptions};
 
     ///
-    /// fn main() {
-    ///    let mut ass_file = ass_parser::AssFile::from_file("subtitles.ass".to_string());
+    /// fn main() Result<(), std::io::Error>{
+    ///    let mut ass_file = ass_parser::AssFile::from_file("subtitles.ass".to_string())?;
     ///    ass_file.components.script 
     ///        .set_scripttype("v4.00+".to_string())
     ///        .set_playresx("384".to_string())
